c1c87afb5b78a00f0cde6fca0abbb5bda99ce824
kelvin dizon <kidswoosh@icloud.com>
2020-03-16 15:28:44 -0700

    a

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(138, 43, 226);
+
+  strokeWeight(5);
 }
 
 function draw() {
-  background(220);
+
+  if (mouseIsPressed) {
+    stroke(map(mouseX, 0, 600, 0, 255, true));
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+}
+  function keyTyped() {
+
+    if (key === 's') {
+      //save this image
+      saveCanvas('fileName', 'png');
+    
+
+    return false;
+  }
 }
