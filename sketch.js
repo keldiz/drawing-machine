2cd5162d9afa32a2c7e9dd42938bfbb20b083d60
kelvin dizon <kidswoosh@icloud.com>
2020-03-25 16:44:38 -0700

    a

--- sketch.js
+++ sketch.js
@@ -1,25 +1,55 @@
+let array = [];
+let backgroundColor = 200;
+
 function setup() {
   createCanvas(600, 600);
-  background(138, 43, 226);
+  background(backgroundColor);
 
   strokeWeight(5);
+  noFill();
 }
 
 function draw() {
 
   if (mouseIsPressed) {
-    stroke(map(mouseX, 0, 600, 0, 255, true));
-    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
-    line(mouseX, mouseY, pmouseX, pmouseY);
+    backgroundColor -= 5;
+    background(backgroundColor);
+    // line(mouseX, mouseY, pmouseX, pmouseY);
+    array.push([mouseX, mouseY]);
+
+    beginShape();
+    for (let i = 0; i < array.length; i++) {
+      // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+      curveVertex(array[i][0], array[i][1]);
+    }
+    endShape();
   }
-}
   function keyTyped() {
 
     if (key === 's') {
       //save this image
       saveCanvas('fileName', 'png');
-    
+    } else if (key === 'd') {
+      //display image
+      background(255);
+
+      beginShape();
+      for (let i = 0; i < array.length; i++) {
+        // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+        curveVertex(array[i][0], array[i][1])
+      }
+      endShape();
+
+    }
 
     return false;
+
   }
+
+  function mousePressed() {
+    array = [];
+    backgroundColor = 255;
+  }
+
+
 }
