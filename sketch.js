1d0d727be927e2acf4045429899a414c70e774fe
kelvin dizon <kidswoosh@icloud.com>
2020-03-25 17:41:43 -0700

    a

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
 let array = [];
-let backgroundColor = 200;
+let backgroundColor = (80, 189, 206);
+let noiseOffset =  0.0;
+let strokeWidth = 5;
 
 function setup() {
   createCanvas(600, 600);
   background(backgroundColor);
+  drawGrid()
 
-  strokeWeight(5);
-  noFill();
+
 }
 
 function draw() {
@@ -16,40 +18,59 @@ function draw() {
     background(backgroundColor);
     // line(mouseX, mouseY, pmouseX, pmouseY);
     array.push([mouseX, mouseY]);
+    strokeWeight(strokeWidth);
+    noFill();
+    noiseOffset += 0.07;
+    strokeWidth =  noise(noiseOffset) * 30;
+
+    // stroke(map(mouseX, 0, 250, 0, 300, true));
 
     beginShape();
     for (let i = 0; i < array.length; i++) {
       // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
-      curveVertex(array[i][0], array[i][1]);
+      curveVertex(array[i][0], array[i][1])
     }
     endShape();
   }
 
-  function keyTyped() {
+ if (key === 'c') {
+   // clear the image
+   clear();
+ }
 
-    if (key === 's') {
-      //save this image
-      saveCanvas('fileName', 'png');
-    } else if (key === 'd') {
-      //display image
-      background(255);
+}
 
-      beginShape();
-      for (let i = 0; i < array.length; i++) {
-        // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
-        curveVertex(array[i][0], array[i][1]);
-        endShape();
-      }
+function keyTyped() {
+
+  if (key === 's') {
+    // save this image
+    saveCanvas('fileName', 'png');
+  } else if (key === 'd') {
+    // display image
+    background(255);
+
+    beginShape();
+    for (let i = 0; i < array.length; i++) {
+      // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+      curveVertex(array[i][0], array[i][1])
     }
-
-    return false;
+endShape();
 
   }
 
-  function mousePressed() {
-    array = [];
-    backgroundColor = 255;
+  return false;
+
+}
+
+function mousePressed() {
+  array = [];
+  backgroundColor = (80, 189, 206);
+}
+
+function drawGrid(){
+  numCells = 20;
+
+  for (let i = 0; i <= width; i += width / numCells){
+    rect(i,  300, width / numCells, width / numCells)
   }
-
-
 }
